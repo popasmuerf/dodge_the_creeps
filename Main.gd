dfaef55a34da26a1fb6f1e0d7401bf0aa45d816d
Mr. Black <mrblack@pop-os.localdomain>
2020-09-13 21:38:51 -0400

    Added HUD. Todo: -Fix ready messag display
    	deleted:    .mono/metadata/ide_messaging_meta.txt
    	new file:   HUD.gd
    	modified:   HUD.tscn
    	modified:   Main.gd
    	modified:   Main.tscn

--- Main.gd
+++ Main.gd
@@ -21,12 +21,14 @@ func _ready():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
@@ -35,6 +37,7 @@ func _on_StartTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_MobTimer_timeout():
